70db472b28552fb13162ef222886bda9e173dc46
vincenz5 <vincenzbuhler@gmail.com>
2019-11-15 01:04:06 -0500

    Added all main components and edited up prof component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LandingComponent } from './landing/landing.component';
+import { ProfComponent } from './prof/prof.component';
+import { WritingComponent } from './writing/writing.component';
+import { PhotoComponent } from './photo/photo.component';
+import { SoftwareComponent } from './software/software.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: 'landing', pathMatch: 'full' },
+  { path: '', component: LandingComponent },
+  { path: 'prof', component: ProfComponent },
+  { path: 'writing', component: WritingComponent },
+  { path: 'photo', component: PhotoComponent },
+  { path: 'software', component: SoftwareComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
 import { TopbarComponent } from './topbar/topbar.component';
+import { ProfComponent } from './prof/prof.component';
+import { WritingComponent } from './writing/writing.component';
+import { PhotoComponent } from './photo/photo.component';
+import { SoftwareComponent } from './software/software.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
-    TopbarComponent
+    TopbarComponent,
+    ProfComponent,
+    WritingComponent,
+    PhotoComponent,
+    SoftwareComponent
   ],
   imports: [
     BrowserModule,
