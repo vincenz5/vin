6c56d5b8c824712860dca39bb1b65036fb4557bc
vincenz5 <vincenzbuhler@gmail.com>
2019-11-14 21:13:35 -0500

    Removed Angular theme and started website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { TopbarComponent } from './topbar/topbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    TopbarComponent
   ],
   imports: [
     BrowserModule,
